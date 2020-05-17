d0ac7cce9d824462dabbe412200cbbbc184c071a
Calvin Tandra <calvin.td43@gmail.com>
2020-05-17 21:47:37 +0700

    wrap up proyek

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,9 @@
-const ref = document.querySelectorAll(".ref")
+const refs = document.querySelectorAll(".ref");
+for (let ref of refs) {
+  ref.addEventListener('click', function(event) {
+    var conf = confirm("Apakah kamu yakin ingin meninggalkan halaman ini?");
+    if (!conf) {
+      event.preventDefault();
+    }
+  })
+}
